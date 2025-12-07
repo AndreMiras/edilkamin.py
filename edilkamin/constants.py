3c4796124ab3a497ae4d97ac5f8a5643ec923b81
Julien Collas <jul.collas@gmail.com>
2025-12-07 14:56:11 +0100

    change url and cognito token

--- edilkamin/constants.py
+++ edilkamin/constants.py
@@ -1,4 +1,4 @@
-BACKEND_URL = "https://fxtj7xkgc6.execute-api.eu-central-1.amazonaws.com/prod/"
+BACKEND_URL = "https://the-mind-api.edilkamin.com/"
 
 USER_POOL_ID = "eu-central-1_BYmQ2VBlo"
 CLIENT_ID = "7sc1qltkqobo3ddqsk4542dg2h"
